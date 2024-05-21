4cefe41f27d2eb1cc858aef8437acd52e913f526
yule44ka <julia.lutova.2004@mail.ru>
2024-05-21 13:18:43 +0300

    bug fixed

--- database_manager.py
+++ database_manager.py
@@ -38,7 +38,7 @@ def search(name, genre, year_min, year_max, iswatched, country):
     if name == '':
         name = "any"
     if iswatched:
-        query = f"""SELECT filtered_table2.Name, filtered_table2.Genre,
+        query = f"""SELECT DISTINCT filtered_table2.Name, filtered_table2.Genre,
                             filtered_table2.Country, filtered_table2.Year, filtered_table2.Description, filtered_table1.Grade
                         FROM
                             (SELECT * FROM {table2} WHERE (%s = 'any' OR Name LIKE '{name}%') AND (%s = 'any' OR Genre = %s) AND (%s < Year)
@@ -48,7 +48,7 @@ def search(name, genre, year_min, year_max, iswatched, country):
                         ON
                             filtered_table1.Name = filtered_table2.Name"""
     else:
-        query = f"""SELECT filtered_table2.Name, filtered_table2.Genre,
+        query = f"""SELECT DISTINCT filtered_table2.Name, filtered_table2.Genre,
                                     filtered_table2.Country, filtered_table2.Year, filtered_table2.Description, filtered_table1.Grade
                                 FROM
                                     (SELECT * FROM {table2} WHERE (%s = 'any' OR Name LIKE '{name}%') AND (%s = 'any' OR Genre = %s) AND (%s < Year)
@@ -59,20 +59,24 @@ def search(name, genre, year_min, year_max, iswatched, country):
                                     filtered_table1.Name = filtered_table2.Name"""
 
     connection, cursor = open_connection()
+
     cursor.execute(query, (name, genre, genre, int(year_min), int(year_max), country, country))
+
     result = cursor.fetchall()  # Fetch results before closing cursor
+
     cursor.close()
     connection.close()
+
     result_dicts = []
-    result.sort()
-    for x in result[0: len(result): 2]:
+
+    for movie in result:
         new_d = {
-            "title": x[0],
-            "genre": x[1],
-            "country": x[2],
-            "year": x[3],
-            "description": x[4],
-            "grade": x[5],
+            "title": movie[0],
+            "genre": movie[1],
+            "country": movie[2],
+            "year": movie[3],
+            "description": movie[4],
+            "grade": movie[5],
         }
         result_dicts.append(new_d)
     return jsonify(results=result_dicts)
